f04e7329d6e51755dfc615c3c389aee762e5bdf8
Luke Schierer <luke@schierer.org>
2024-10-20 04:09:05 -0400

    build: this should allow to test both the front and back ends

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { gracile } from "@gracile/gracile/plugin";
 import { defineConfig } from "vite";
 import path from "path";
+import devServer from "@hono/vite-dev-server";
+import bunAdapter from "@hono/vite-dev-server/bun";
 
 export default defineConfig({
   server: {
@@ -13,9 +15,16 @@ export default defineConfig({
       "@components": path.resolve(__dirname, "./src/components"),
       "@styles": path.resolve(__dirname, "./src/styles"),
       "@schemas": path.resolve(__dirname, "./src/schemas"),
+      "@templates": path.resolve(__dirname, "./src/templates"),
     },
   },
   plugins: [
+    devServer({
+      entry: "./server.ts",
+      injectClientScript: false,
+      adapter: bunAdapter,
+      exclude: ["/api/*"],
+    }),
     gracile({
       output: "server",
 
