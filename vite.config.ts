616ab24c801c2e15e26af15f7f51140911dc689a
Luke Schierer <luke@schierer.org>
2024-10-20 03:32:32 -0400

    build: set up aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,32 @@
-import { gracile } from '@gracile/gracile/plugin';
-import { defineConfig } from 'vite';
+import { gracile } from "@gracile/gracile/plugin";
+import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
-	server: {
-		port: 3030,
-	},
+  server: {
+    port: 3030,
+  },
+  resolve: {
+    alias: {
+      "@src": path.resolve(__dirname, "./src"),
+      "@data": path.resolve(__dirname, "./data"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@schemas": path.resolve(__dirname, "./src/schemas"),
+    },
+  },
+  plugins: [
+    gracile({
+      output: "server",
 
-	plugins: [
-		gracile({
-			output: 'server',
-
-			dev: {
-				locals: (_context) => {
-					return {
-						requestId: crypto.randomUUID(),
-						userEmail: 'admin@admin.home.arpa',
-					};
-				},
-			},
-		}),
-	],
+      dev: {
+        locals: (_context) => {
+          return {
+            requestId: crypto.randomUUID(),
+            userEmail: "admin@admin.home.arpa",
+          };
+        },
+      },
+    }),
+  ],
 });
