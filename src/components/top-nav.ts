70db894eee8f2008350fbd10b410c28486f32278
Luke Schierer <lschiere@amazon.com>
2024-10-21 18:13:37 -0400

    feat: top nav
    I have a logo and a link home

--- src/components/top-nav.ts
+++ src/components/top-nav.ts
@@ -1,5 +1,6 @@
-import { unsafeCSS, html, LitElement, type PropertyValues } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { nothing, unsafeCSS, html, LitElement, type PropertyValues } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
+import { when } from "lit/directives/when.js";
 
 import globalStyles from "@styles/global.css?inline";
 import topNavStyles from "./top-nav.css?inline";
@@ -12,10 +13,36 @@ export class TopNav extends LitElement {
 
   static styles = [unsafeCSS(globalStyles), unsafeCSS(topNavStyles)];
 
+  @property({ type: String })
+  public SiteLogoFilename: string | undefined = import.meta.env.VITE_SiteLogo;
+
+  @property({ type: String })
+  public SiteName: string = import.meta.env.VITE_SiteName ?? "";
+
+  @state({ type: String })
+  private Selected: string = "home";
+
   override render() {
     return html`
-      <div class="TopNav">
-        <p>TopNav</p>
+      <div id="TopNav" class="bg-SpectrumCyan-700 flex">
+        <div id="LogoContainer" class="flex items-stretch flex-top-left">
+          <div id="Logo" class="flex-1">
+            ${when(
+              this.SiteLogoFilename !== undefined,
+              () => {
+                return html`<img src="${this.SiteLogoFilename}"></img>`;
+              },
+              () => nothing
+            )}
+          </div>
+          <div id="home" class=" v-full items-stretch flex-top-left">
+            <span
+              class="v-full spectrum-Heading spectrum-Heading--light text-4xl"
+            >
+              <a href="/">Home</a>
+            </span>
+          </div>
+        </div>
       </div>
     `;
   }
