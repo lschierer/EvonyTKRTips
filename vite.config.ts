73adc21ba3237ade9354820eacb13ee46b374cab
Luke Schierer <luke@schierer.org>
2024-10-21 01:14:33 -0400

    feat: vite/bun configuration
    current state

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,57 @@
 import { gracile } from "@gracile/gracile/plugin";
+import { viteSvgPlugin } from "@gracile/svg/vite";
+
 import { defineConfig } from "vite";
 import path from "path";
 
-export default defineConfig({
-  server: {
-    port: 3030,
-  },
-  preview: {
-    //Make sure I do not grab 3030 while in preview, because I will need it for the dev server.
-    port: 2020,
-  },
-  resolve: {
-    alias: {
-      "@src": path.resolve(__dirname, "./src"),
-      "@data": path.resolve(__dirname, "./data"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@styles": path.resolve(__dirname, "./src/styles"),
-      "@schemas": path.resolve(__dirname, "./src/schemas"),
-      "@templates": path.resolve(__dirname, "./src/templates"),
-    },
-  },
-  plugins: [
-    gracile({
-      output: "server",
+const aliases = {
+  "@assets": path.resolve(__dirname, "./src"),
+  "@src": path.resolve(__dirname, "./src"),
+  "@data": path.resolve(__dirname, "./data"),
+  "@components": path.resolve(__dirname, "./src/components"),
+  "@styles": path.resolve(__dirname, "./src/styles"),
+  "@schemas": path.resolve(__dirname, "./src/schemas"),
+  "@templates": path.resolve(__dirname, "./src/templates"),
+};
 
-      dev: {
-        locals: (_context) => {
-          return {
-            requestId: crypto.randomUUID(),
-            userEmail: "admin@admin.home.arpa",
-          };
-        },
+export default defineConfig(({ command, mode }) => {
+  if (command === "serve") {
+    return {
+      base: "https://www.evonytkrtips.net/",
+      server: {
+        port: 3030,
       },
-    }),
-  ],
+      resolve: {
+        alias: aliases,
+      },
+      plugins: [
+        gracile({
+          output: "server",
+        }),
+      ],
+    };
+  } else {
+    return {
+      server: {
+        port: 3030,
+      },
+      resolve: {
+        alias: aliases,
+      },
+      plugins: [
+        viteSvgPlugin({ plugins: ["preset-default"] }),
+        gracile({
+          output: "server",
+          dev: {
+            locals: (_context) => {
+              return {
+                requestId: crypto.randomUUID(),
+                userEmail: "admin@admin.home.arpa",
+              };
+            },
+          },
+        }),
+      ],
+    };
+  }
 });
