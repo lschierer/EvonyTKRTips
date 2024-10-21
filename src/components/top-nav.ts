38bd916a4871249fe11c2cf228de101d63dfdb86
Luke Schierer <lschiere@amazon.com>
2024-10-21 18:34:16 -0400

    feat: top nav
    a stab at site sections

--- src/components/top-nav.ts
+++ src/components/top-nav.ts
@@ -1,6 +1,7 @@
 import { nothing, unsafeCSS, html, LitElement, type PropertyValues } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
 import { when } from "lit/directives/when.js";
+import { classMap } from "lit/directives/class-map.js";
 
 import globalStyles from "@styles/global.css?inline";
 import topNavStyles from "./top-nav.css?inline";
@@ -22,7 +23,29 @@ export class TopNav extends LitElement {
   @state({ type: String })
   private Selected: string = "home";
 
+  @property({ type: Array })
+  public SiteSections: string[] = new Array<string>();
+
   override render() {
+    let SectionTemplate = html``;
+    this.SiteSections.forEach((section) => {
+      const dynamicClasses = {
+        selected: !section.localeCompare(this.Selected),
+      };
+      const sectionLink = `/${section.replaceAll(" ", "").toLowerCase()}/`;
+      SectionTemplate = html`${SectionTemplate}
+        <div
+          id="${section.replaceAll(" ", "")}"
+          class="${classMap(dynamicClasses)} v-full items-stretch flex-top-left"
+        >
+          <span
+            class="v-full spectrum-Heading spectrum-Heading--light text-4xl"
+          >
+            <a href="${sectionLink}">${section}</a>
+          </span>
+        </div> `;
+    });
+
     return html`
       <div id="TopNav" class="bg-SpectrumCyan-700 flex">
         <div id="LogoContainer" class="flex items-stretch flex-top-left">
@@ -30,7 +53,7 @@ export class TopNav extends LitElement {
             ${when(
               this.SiteLogoFilename !== undefined,
               () => {
-                return html`<img src="${this.SiteLogoFilename}"></img>`;
+                return html`<img src="${this.SiteLogoFilename!}"></img>`;
               },
               () => nothing
             )}
@@ -42,6 +65,9 @@ export class TopNav extends LitElement {
               <a href="/">Home</a>
             </span>
           </div>
+        </div>
+        <div id="SiteSections" class="flex items-stretch flex-top-right">
+          ${SectionTemplate}
         </div>
       </div>
     `;
