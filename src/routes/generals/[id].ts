f2afc968e79fef0905ab29590a9baae87a5c4ac5
Luke Schierer <luke@schierer.org>
2024-10-22 01:16:27 -0400

    fix: use the right Params

--- src/routes/generals/[id].ts
+++ src/routes/generals/[id].ts
@@ -3,7 +3,7 @@ import { defineRoute } from "@gracile/gracile/route";
 import { document } from "@templates/base";
 
 export default defineRoute({
-  document: (context) => document(context),
+  document: (context) => document({ ...context, section: "Generals" }),
 
   template: async (context) => html` <h1>${context.params.id}</h1> `,
 });
