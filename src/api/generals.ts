065b074ab8e705aba6a6eaf7a5b1879be6fc2a8e
Luke Schierer <luke@schierer.org>
2024-10-22 01:50:27 -0400

    feat: hono RPC
    trying to use hono RPC as intended

--- src/api/generals.ts
+++ src/api/generals.ts
@@ -1,17 +1,29 @@
 import { Hono } from "hono";
+import { type } from "arktype";
+import { arktypeValidator } from "@hono/arktype-validator";
 
 import * as evonyTypes from "@schemas/evony";
 
 const app = new Hono<{ Variables: Gracile.Locals }>();
 
-app.get("/:id", (c) => {
-  return c.json(
-    evonyTypes.GeneralTypes.General({
-      name: "My Name",
-      id: crypto.randomUUID(),
-      level: 45,
-    })
-  );
-});
+const error = c.json("Invalid", 400);
+
+app.post(
+  "/:id",
+  arktypeValidator("json", evonyTypes.GeneralTypes.General), (c) => {
+    const data = c.req.valid('json');
+    if(data instanceof type.errors) {
+      return c.json({
+        success: false,
+        message: `${data.summary}`,
+        status: 400
+      });
+    }
+    else {
+
+    }
+
+  })
+);
 
 export default app;
--- src/routes/generals/[id].ts
+++ src/routes/generals/[id].ts
@@ -2,6 +2,12 @@ import { html } from "lit";
 import { defineRoute } from "@gracile/gracile/route";
 import { document } from "@templates/base";
 
+import * as gracile from "@gracile/gracile/hono";
+import { type AppType } from "@server";
+import { hc } from "hono/client";
+
+const client = hc<AppType>("http://localhost:8787/");
+
 export default defineRoute({
   document: (context) => document({ ...context, section: "Generals" }),
 
