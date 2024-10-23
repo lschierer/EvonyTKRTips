88cffcb8b762ccdd90092e354134d9667ef8dc57
Luke Schierer <luke@schierer.org>
2024-10-23 02:30:44 -0400

    feat: route debugging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import { Hono } from "hono";
-import { serve } from "@hono/node-server";
-import { serveStatic } from "@hono/node-server/serve-static";
+import { logger } from "hono/logger";
+import { poweredBy } from "hono/powered-by";
 import { hc } from "hono/client";
 
 import api from "./api/api";
 
 const app = new Hono();
+app.use(poweredBy());
+app.use(logger());
 
 app.get("/test", (c) => c.text("hello world"));
 const APIRoute = app.route("/api", api);
