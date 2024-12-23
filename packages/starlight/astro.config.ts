a033be8e043a9e6e4dddad2229ef88a348119dfe
Luke Schierer <lschiere@amazon.com>
2024-12-23 17:18:49 -0500

    feat: styling
    nearly identical styling from the HP site, it seems to be my theme ;-)

--- packages/starlight/astro.config.ts
+++ packages/starlight/astro.config.ts
@@ -1,28 +1,48 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-// https://astro.build/config
+import { TopLevelSections } from "./src/lib/topLevelSections";
+
+import mdx from "@astrojs/mdx";
+
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://www.evonytkrtips.net",
+  trailingSlash: "always",
+  integrations: [
+    starlight({
+      title: "Evony TKR Tips",
+      logo: {
+        src: "./src/assets/Logo.svg",
+        replacesTitle: true,
+      },
+      social: {
+        github: "https://github.com/lschierer/EvonyTKRTips",
+      },
+      tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 4 },
+      pagefind: false,
+      customCss: [
+        "@spectrum-css/tokens/dist/index.css",
+        "@spectrum-css/typography/dist/index.css",
+        "@spectrum-css/link/dist/index.css",
+        "@spectrum-css/page/dist/index.css",
+        "./src/styles/global.css",
+      ],
+      components: {
+        Header: "./src/components/Header.astro",
+        ThemeProvider: "./src/components/ThemeProvider.astro",
+        PageFrame: "./src/components/PageFrame.astro",
+      },
+      sidebar: TopLevelSections.options.map((section) => {
+        return {
+          label: section.replaceAll("_", " "),
+          autogenerate: {
+            directory: section.replaceAll(" ", ""),
+            collapsed: true,
+          },
+          collapsed: true,
+        };
+      }),
+    }),
+    mdx(),
+  ],
 });
