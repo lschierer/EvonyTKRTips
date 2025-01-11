96302a06ee3a173d4069a914add40882cd4a740c
Luke Schierer <lschiere@amazon.com>
2025-01-10 21:38:03 -0500

    feat: generals tables
    struggling to virtualize the combination of tanstack and spectrum

--- packages/starlight/src/components/generals/generalTable.ts
+++ packages/starlight/src/components/generals/generalTable.ts
@@ -10,7 +10,9 @@ import {
   type TableState,
   type TableOptions,
   type Header,
+  type Row,
 } from "@tanstack/lit-table";
+import { VirtualizerController } from "@tanstack/lit-virtual";
 
 import {
   LitElement,
@@ -20,10 +22,14 @@ import {
   type CSSResultGroup,
   unsafeCSS,
   type TemplateResult,
+  nothing,
 } from "lit";
 import { repeat } from "lit/directives/repeat.js";
 import { customElement, property, state } from "lit/decorators.js";
 import { ifDefined } from "lit/directives/if-defined.js";
+import { createRef, ref, type Ref } from "lit/directives/ref.js";
+import { styleMap } from "lit/directives/style-map.js";
+import { classMap } from "lit/directives/class-map.js";
 
 import { StoreController } from "@nanostores/lit";
 import { subscribeKeys } from "nanostores";
@@ -69,7 +75,7 @@ import * as stores from "./store";
 import columns from "./columns";
 import { GeneralAscending } from "@schemas/ascending";
 
-const DEBUG = false;
+const DEBUG = true;
 
 @customElement("table-element")
 export default class TableElement extends LitElement {
@@ -115,10 +121,13 @@ export default class TableElement extends LitElement {
   @state()
   private _sorting: SortingState = [];
 
-  @state()
-  private table: Table<GeneralPair> | null = null;
-
   private tableController = new TableController<GeneralPair>(this);
+  private rowVirtualizerController: VirtualizerController<
+    Element,
+    Element
+  > | null = null;
+  private tableContainerRef: Ref = createRef();
+
   private sortKey: string = "primary";
   private sortDirection: string = "asc";
 
@@ -211,51 +220,109 @@ export default class TableElement extends LitElement {
     }
   }
 
-  static override styles?: CSSResultGroup = [
-    unsafeCSS(SpectrumTableCSS),
-    unsafeCSS(GeneralsCSS),
-  ];
+  override connectedCallback(): void {
+    stores.pairs.listen((v, o) => {
+      if (DEBUG) {
+        console.log(`connectedCallback sees a change to pairs`);
+        console.log(
+          `this.tableContainerRef.value is ${this.tableContainerRef.value ? "present" : "not present"}`
+        );
+      }
+      this.rowVirtualizerController = new VirtualizerController(this, {
+        count: v.length,
+        getScrollElement: () => this.tableContainerRef.value!,
+        estimateSize: () => 33,
+        overscan: 5,
+      });
+      if (DEBUG) {
+        console.log(
+          `connectedCallback rowVirtualizerController shows ${this.rowVirtualizerController.getVirtualizer().getVirtualItems().length} virtual items`
+        );
+      }
+    });
+    super.connectedCallback();
+  }
 
   protected tableHead(table: Table<GeneralPair>): TemplateResult {
+    const theadStyles = {
+      "grid-row": `1 / ${table.getHeaderGroups().length + 1}`,
+    };
     return html`
-      <thead class="spectrum-Table-head">
-        ${table.getHeaderGroups().map((headerGroup) => {
+      <thead
+        class="not-content spectrum-Table-head"
+        style=${styleMap(theadStyles)}
+      >
+        ${table.getHeaderGroups().map((headerGroup, index) => {
+          const trStyle = {
+            "grid-column": `${index + 1} / ${index + 1 + headerGroup.headers.length}`,
+          };
           return html`
             <tr key=${headerGroup.id}>
               ${headerGroup.headers.map(
                 (header: Header<GeneralPair, unknown>) => {
+                  /*
+                   * there doesn't seem to be an API for the *current* just the *next*
+                   * the state machine goes ascending -> decending -> other -> acending (loop)
+                   * if can sort, test further.
+                   * if next is ascending, I am currently on "other"
+                   * If next is not ascending it could be decending or "other".
+                   * if next is decending, I am currently on ascending.
+                   * if next is niether ascending nor decending, I am currently on decending.
+                   * else I could not sort, so return false
+                   */
+                  const thclasses = {
+                    "is-sortable": header.column.getCanSort() ? true : false,
+                    "is-sorted-asc": header.column.getCanSort()
+                      ? header.column.getNextSortingOrder() === "asc"
+                        ? false
+                        : header.column.getNextSortingOrder() === "desc"
+                          ? true
+                          : false
+                      : false,
+                    "is-sorted-desc": header.column.getCanSort()
+                      ? header.column.getNextSortingOrder() === "asc"
+                        ? false
+                        : header.column.getNextSortingOrder() === "desc"
+                          ? false
+                          : true
+                      : false,
+                  };
+                  const thStyles = {
+                    "grid-column": `${header.index + 1} / ${header.index + 1 + header.colSpan}`,
+                  };
                   return html`
                     <th
                       key=${header.id}
                       colspan=${header.colSpan}
-                      class="spectrum-Table-headCell is-sortable"
+                      class="not-content spectrum-Table-headCell ${classMap(
+                        thclasses
+                      )}"
+                      ,
+                      style=${styleMap(thStyles)}
+                      aria-sort="${header.column.getCanSort()
+                        ? header.column.getNextSortingOrder() === "asc"
+                          ? "other"
+                          : header.column.getNextSortingOrder() === "desc"
+                            ? "ascending"
+                            : "descending"
+                        : "none"}"
+                      @click="${header.column.getToggleSortingHandler()}"
                     >
                       ${header.isPlaceholder
                         ? null
-                        : html` <div
-                            title=${ifDefined(
-                              header.column.getCanSort()
-                                ? header.column.getNextSortingOrder() === "asc"
-                                  ? "Sort ascending"
-                                  : header.column.getNextSortingOrder() ===
-                                      "desc"
-                                    ? "Sort descending"
-                                    : "Clear sort"
-                                : undefined
-                            )}
-                            @click="${header.column.getToggleSortingHandler()}"
-                            style="cursor: ${header.column.getCanSort()
-                              ? "pointer"
-                              : "not-allowed"}"
-                          >
-                            ${flexRender(
-                              header.column.columnDef.header,
-                              header.getContext()
-                            )}
-                            ${{ asc: " 🔼", desc: " 🔽" }[
-                              header.column.getIsSorted() as string
-                            ] ?? null}
-                          </div>`}
+                        : html`
+                            <span
+                              class="tableHeader spectrum-Table-columnTitle"
+                            >
+                              ${flexRender(
+                                header.column.columnDef.header,
+                                header.getContext()
+                              )}
+                              ${{ asc: " 🔼", desc: " 🔽" }[
+                                header.column.getIsSorted() as string
+                              ] ?? null}
+                            </span>
+                          `}
                     </th>
                   `;
                 }
@@ -267,41 +334,142 @@ export default class TableElement extends LitElement {
     `;
   }
 
-  protected tableBody(table: Table<GeneralPair>): TemplateResult {
+  protected tableBody(
+    table: Table<GeneralPair>,
+    rows: Row<GeneralPair>[]
+  ): TemplateResult {
+    if (DEBUG) {
+      console.log(`tableBody called with ${rows.length} rows`);
+    }
+    if (this.rowVirtualizerController) {
+      if (DEBUG) {
+        console.log(`tableBody has a rowVirtualizerController`);
+      }
+      const virtualizer = this.rowVirtualizerController.getVirtualizer();
+      const bodyStyles = {
+        "grid-row": `${table.getHeaderGroups().length + 1} / -1`,
+      };
+      return html`
+        <tbody class="spectrum-Table-body" style=${styleMap(bodyStyles)}>
+          ${repeat(
+            this.rowVirtualizerController.getVirtualizer().getVirtualItems(),
+            (item) => item.key,
+            (item) => {
+              const row = rows[item.index];
+              return html`
+                <tr class="spectrum-Table-row" style=${styleMap({})}>
+                  ${repeat(
+                    row.getVisibleCells(),
+                    (cell) => cell.id,
+                    (cell) => {
+                      return html`
+                        <td class="spectrum-Table-cell" style=${styleMap({})}>
+                          ${flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )}
+                        </td>
+                      `;
+                    }
+                  )}
+                </tr>
+              `;
+            }
+          )}
+        </tbody>
+      `;
+    }
     return html`
-      <tbody class="spectrum-Table-body">
-        ${table.getSortedRowModel().rows.map(
-          (row) => html`
-            <tr class="spectrum-Table-row">
-              ${row
-                .getVisibleCells()
-                .map(
-                  (cell) => html`
-                    <td class="spectrum-Table-cell">
-                      ${flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </td>
-                  `
-                )}
-            </tr>
-          `
-        )}
+      <tbody>
+        fallback
       </tbody>
     `;
   }
+
+  static override styles?: CSSResultGroup = [
+    unsafeCSS(SpectrumTableCSS),
+    unsafeCSS(GeneralsCSS),
+    css`
+      div#tableContainer.tableContainer {
+        display: grid;
+        overflow-x: scroll;
+        overflow: scroll;
+        margin-top: 1rem;
+        width: minmax(max-content, 50vw);
+        height: 50vh;
+        position: "relative";
+      }
+
+      div#tableContainer.tableContainer
+        table.spectrum-Table.spectrum-Table-main {
+        grid-column: 1/-1;
+        grid-row: 1/-1;
+        display: grid;
+        grid-auto-flow: row;
+        grid-template-columns: subgrid;
+        grid-template-rows: subgrid;
+      }
+
+      thead.spectrum-Table-head {
+        grid-column: 1/-1;
+        display: table;
+        height: fit-content;
+        background-color: var(--spectrum-table-header-background-color);
+        z-index:;
+      }
+
+      thead.spectrum-Table-head tr {
+        border-bottom-width: var(--spectrum-table-border-width);
+        border-bottom-style: solid;
+        border-bottom-color: var(--spectrum-table-border-color);
+      }
+
+      thead.spectrum-Table-head tr th.spectrum-Table-headCell {
+        text-align: center;
+        border-top: 0px;
+        border-bottom: 0px;
+        height: 100%;
+      }
+
+      th div {
+        height: max-content;
+        overflow: visible;
+        word-wrap: normal;
+      }
+
+      tbody.spectrum-Table-body tr.spectrum-Table-row {
+        grid-column: 1/-1;
+        display: table;
+        width: 100%;
+        height: max-content;
+      }
+
+      tbody.spectrum-Table-body tr.spectrum-Table-row td.spectrum-Table-cell {
+        height: fit-content;
+      }
+    `,
+  ];
 
   private index = 0;
   protected override render() {
     if (DEBUG) {
       console.log(`TableElement render start ${this.index++}`);
     }
+
+    const fallback = html`
+      <div
+        class="tableContainer spectrum-Table--sizeM spectrum-Table--empasized"
+        ${ref(this.tableContainerRef)}
+      >
+        pending data
+      </div>
+    `;
+
     if (
       !Array.isArray(this.pairsController.value) ||
       this.pairsController.value.length == 0
     ) {
-      return html`pending data`;
+      return fallback;
     } else {
       if (DEBUG) {
         console.log(
@@ -339,42 +507,34 @@ export default class TableElement extends LitElement {
             columnVisibility: stores.columnVisibility.get(),
           },
         });
-        const state = { ...table.initialState, ...this._tableState };
-        const oldOptions = table.options;
-        const newOptions: TableOptions<GeneralPair> = {
-          columns,
-          data: stores.pairs.value,
-          manualSorting: false,
-          onSortingChange: oldOptions.onSortingChange,
-          state: oldOptions.state,
-          onStateChange: oldOptions.onStateChange,
-          getSortedRowModel: oldOptions.getSortedRowModel,
-          getCoreRowModel: oldOptions.getCoreRowModel,
-        };
-        //@ts-expect-error
-        table.setOptions(newOptions);
 
-        if (DEBUG) {
-          console.log(
-            `table has ${table.getRowModel().rows.length} rows in render`
-          );
-        }
-        return html`
-          <div
-            class="tableContainer spectrum-Table spectrum-Table-scroller spectrum-Table--sizeM spectrum-Table--empasized"
-          >
-            <table
-              class="spectrum-Table-main spectrum-Table--sizeM spectrum-Table--emphasized"
+        const { rows } = table.getRowModel();
+        if (this.rowVirtualizerController) {
+          const virtualizer = this.rowVirtualizerController.getVirtualizer();
+          const tableStyle = {
+            "grid-template-columns": `repeat(${rows[0].getVisibleCells().length}, 1fr)`,
+            "grid-template-rows": `repeat(${table.getHeaderGroups().length + rows.length}, minmax(max-content,5rem)
+            )`,
+          };
+          return html`
+            <div
+              id="tableContainer"
+              class="not-content tableContainer spectrum-Table--sizeM spectrum-Table--empasized"
+              style="${styleMap(tableStyle)}"
+              ${ref(this.tableContainerRef)}
             >
-              ${this.tableHead(table)} ${this.tableBody(table)}
-            </table>
-          </div>
-          ${DEBUG
-            ? html`<pre>${JSON.stringify(this._sorting, null, 2)}</pre>`
-            : ""}
-        `;
+              <table
+                class="spectrum-Table spectrum-Table-main spectrum-Table--sizeM spectrum-Table--emphasized"
+              >
+                ${this.tableHead(table)} ${this.tableBody(table, rows)}
+              </table>
+            </div>
+          `;
+        } else {
+          return fallback;
+        }
       } else {
-        return html`pending data`;
+        return fallback;
       }
     }
   }
