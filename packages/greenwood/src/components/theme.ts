65e74c06aab8b016ea616fc332eb67dff41ab08e
Luke Schierer <lschiere@amazon.com>
2025-01-15 17:45:21 -0500

    feat: theming
    base spectrum theme set

---
+++ packages/greenwood/src/components/theme.ts
@@ -0,0 +1,45 @@
+const DEBUG = true;
+
+export default class ThemeComponent extends HTMLElement {
+  protected connectedCallback() {
+    const storedTheme =
+      typeof localStorage !== "undefined" &&
+      localStorage.getItem("greenwood-theme");
+
+    const theme =
+      storedTheme ||
+      (window.matchMedia("(prefers-color-scheme: light)").matches
+        ? "light"
+        : "dark");
+
+    if (DEBUG) {
+      console.log(
+        `ThemeComponent thinks theme is ${theme}, storedTheme is ${storedTheme}`
+      );
+    }
+
+    document.querySelectorAll("html").forEach((htmlTag) => {
+      if (!theme.localeCompare("light") || !theme.localeCompare("dark")) {
+        const scale = "medium";
+        if (htmlTag != undefined) {
+          if (!htmlTag.classList.contains("spectrum")) {
+            htmlTag.classList.add("spectrum");
+          }
+          if (!htmlTag.classList.contains(theme)) {
+            if (!theme.localeCompare("light")) {
+              htmlTag.classList.add("spectrum--light");
+              htmlTag.classList.remove("spectrum--dark");
+            } else {
+              htmlTag.classList.remove("spectrum--light");
+              htmlTag.classList.add("spectrum--dark");
+            }
+          }
+          if (!htmlTag.classList.contains(scale)) {
+            htmlTag.classList.add(scale);
+          }
+        }
+      }
+    });
+  }
+}
+customElements.define("theme-component", ThemeComponent);
