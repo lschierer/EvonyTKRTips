302952ae25d9a1d59d1a008a2f7a98bfd1e2b30c
Luke Schierer <lschiere@amazon.com>
2025-02-07 21:14:24 -0500

    feat: reactive monster reports replacement
    trying to get tabs to work, they aren't displaying correctly yet.
    However, this is closer to what I want in terms of site structure and sanity

--- packages/greenwood/src/components/Monsters/Simulator/index.ts
+++ packages/greenwood/src/components/Monsters/Simulator/index.ts
@@ -1,71 +1,62 @@
-import { LitElement, html } from "lit";
+import { LitElement, html, unsafeCSS, css } from "lit";
 
 import "./BuffTable.ts";
 import "./MarchTable.ts";
 import "./MonsterOrder.ts";
 
+import SpectrumCSStabs from "@spectrum-css/tabs/index.css" with { type: "css" };
+import "iconify-icon";
+
 export default class MonsterSimulator extends LitElement {
+  static override styles = [
+    unsafeCSS(SpectrumCSStabs),
+    css`
+      div.spectrum-Tabs {
+        background-color: var(--spectrum-green-background-color-default);
+      }
+    `,
+  ];
   override render() {
     return html`
-      <article class="page">
-        <h2 class="spectrum-Heading spectrum-Heading--sizeXL">
-          Credits and Instructions
-        </h2>
-        <section class="credits">
-          This is based almost entirely on the spreadsheet created by
-          <a href="https://www.youtube.com/@DerrickDefies">Derrick Defies</a>
-          and provided by him in his Discord server. I have attempted to
-          faithfully recreate it here.
-        </section>
-        <section class="instructions">
-          <ol>
-            <li>Attack the Alliance monster with a full mounted march.</li>
-            <li>
-              Insert the damage from the report in the "Boss Damage" field.
-            </li>
-            <li>
-              Input your Troop type and March size in the indicated fields.
-            </li>
-            <li>
-              Adjust the Alliance Boss modifier.
-              <ul>
-                <li>
-                  The original spreadsheet is unclear how to figure out the
-                  *correct* value to put here.
-                </li>
-                <li>Per the original, 100% works.</li>
-                <li>
-                  Per the original, do not use 20%, Evony as a bug in their
-                  programming.
-                </li>
-              </ul>
-            </li>
-            <li>
-              Accurately fill out your flat buffs for that troop type. This goes
-              on line 15, and you will only fill in 3 of the available nine
-              columns (one attack column, one defense column, one hp column).
-              Ensure you look at each of the following (and anything I might
-              have missed)
-              <ul>
-                <li>Refines for anything that can be refined.</li>
-                <li>Covenants</li>
-                <li>Flexible specialties</li>
-                <li>Castles that might have buffs simply for owning them</li>
-                <li>Detail Tab</li>
-                <li>General Skins</li>
-              </ul>
-            </li>
-          </ol>
-        </section>
-        <h2 class="spectrum-Heading spectrum-Heading--sizeXL">The Simulator</h2>
-        <section class="simulator">
-          <div>
-            <buff-table></buff-table>
-            <march-table></march-table>
-            <monster-order></monster-order>
+      <div>
+        <div class="spectrum-Tabs  spectrum-Tabs--horizontal">
+          <div
+            tabindex="0"
+            class="spectrum-Tabs-item is-selected"
+            id="tab-item-1"
+          >
+            <iconify-icon
+              icon="ion:folder-open-outline"
+              focusable="false"
+              aria-hidden="true"
+              role="img"
+              class="spectrum-Icon spectrum-Icon--sizeM "
+            ></iconify-icon>
+            <span class="spectrum-Tabs-itemLabel"> Tab 1 </span>
+            <div
+              class="spectrum-Tabs-selectionIndicator"
+              style="inline-size:100%;"
+            ></div>
           </div>
-        </section>
-      </article>
+
+          <div tabindex="0" class="spectrum-Tabs-item " id="tab-item-2">
+            <iconify-icon
+              icon="ion:folder-open-outline"
+              focusable="false"
+              aria-hidden="true"
+              role="img"
+              class="spectrum-Icon spectrum-Icon--sizeM "
+            ></iconify-icon>
+            <span class="spectrum-Tabs-itemLabel"> Tab 2 </span>
+          </div>
+        </div>
+
+        <div class="tab-1-content">
+          <buff-table></buff-table>
+          <march-table></march-table>
+          <monster-order></monster-order>
+        </div>
+      </div>
     `;
   }
 }
