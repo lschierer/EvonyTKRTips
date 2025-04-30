d697553602db2156c7f7b457fb5e9ee43c88f0b6
Luke Schierer <lschiere@amazon.com>
2025-04-30 18:41:23 -0400

    fix: fix an import lets the sidebar fully work, remove a duplicate element from the layout merge

--- packages/greenwood/src/components/SideBar.ts
+++ packages/greenwood/src/components/SideBar.ts
@@ -2,7 +2,7 @@ import { type ClientNavItem } from "@evonytkrtips/schemas";
 import { type Page } from "@greenwood/cli";
 import { getContent } from "@greenwood/cli/src/data/client.js";
 
-import SideNavCSS from "../styles/sidebar.css" with { type: "css" };
+import SideNavCSS from "/node_modules/@evonytkrtips/assets/dist/styles/sidebar.css" with { type: "css" };
 
 import debugFunction from "../lib/debug.ts";
 const DEBUG = debugFunction(new URL(import.meta.url).pathname);
